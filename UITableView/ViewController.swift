48d01780bf514236924033706e8b46d0ee243697
Me <lukagujejiani@gmail.com>
2024-04-12 20:44:14 +0400

    Initial Commit

---
+++ UITableView/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UITableView
+//
+//  Created by Luka Gujejiani on 12.04.24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
